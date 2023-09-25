e0a5ca20f6a5d4aed21e762dc05020231adf6ab1
Reed Trevelyan <reedtrevelyan@gmail.com>
2023-09-24 22:36:01 -0400

    fix answer card css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { useState, ReactNode, SyntheticEvent } from "react";
-import music1 from "./assets/music-1.png";
 import { ComposerWork, parseGuess } from "./composerWork";
+import { puzzles } from "./dailyPuzzle";
 
 function App() {
   const MAX_GUESSES = 5;
 
   const [guesses, setGuesses] = useState<ComposerWork[]>([]);
   const [currentGuess, setCurrentGuess] = useState("");
+
+  const currentPuzzle = puzzles.slice(-1)[0];
 
   const makeGuess = (e: SyntheticEvent | undefined) => {
     if (e) {
@@ -25,13 +27,16 @@ function App() {
     setCurrentGuess("");
   };
 
+  const composerCorrect = (guess: ComposerWork): boolean => guess.composer === currentPuzzle?.puzzleAnswer.composer;
+  const workCorrect = (guess: ComposerWork): boolean => guess.work === currentPuzzle?.puzzleAnswer.work;
+
   const renderGuesses = (): ReactNode => {
     const guessItems = guesses.map((guess, i) => {
       return (
-        <div key={i} className="flex justify-center">
+        <div key={i} className="flex justify-center items-stretch mb-2">
           {renderGuessNumber(i + 1)}
-          {renderComposerCard(guess.composer)}
-          {renderWorkCard(guess.work)}
+          {renderAnswerCard("Composer", guess.composer, "cyan-500", 12, composerCorrect(guess))}
+          {renderAnswerCard("Work", guess.composer, "indigo-500", 14, workCorrect(guess))}
         </div>
       );
     });
@@ -39,7 +44,7 @@ function App() {
     for (let i = 0; i < MAX_GUESSES - guesses.length; i++) {
       // fill remainder with placeholders
       guessItems.push(
-        <div key={i + guesses.length} className="flex justify-center">
+        <div key={i + guesses.length} className="flex justify-center mb-2">
           {renderGuessNumber(i + guesses.length + 1)}
           {renderPlaceholderCard()}
         </div>
@@ -50,60 +55,54 @@ function App() {
   };
 
   const renderGuessNumber = (i: number): ReactNode => (
-    <div className="bg-yellow-400 rounded-lg py-1 px-2 mr-2 my-1 text-blue-800 text-md font-semibold flex flex-col justify-center content-center">
+    <div className="bg-yellow-400 rounded-lg py-1 px-2 mr-2 text-blue-800 text-md font-semibold flex flex-col justify-center content-center">
       <span>{i}</span>
     </div>
   );
 
   const renderPlaceholderCard = (): ReactNode => (
     <>
-      <div className="block max-w-[12rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-cyan-500 mr-2 flex-grow mb-2"></div>
-      <div className="block max-w-[14rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-cyan-500 mr-2 flex-grow mb-2"></div>
+      <div className="block max-w-[12rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-cyan-500 mr-2 flex-grow"></div>
+      <div className="block max-w-[14rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-cyan-500 mr-2 flex-grow"></div>
     </>
   );
 
-  const renderCardTitle = (text: string, correct: boolean = true): ReactNode => (
+  const renderCardTitle = (
+    text: string,
+    correct: boolean = true
+  ): ReactNode => (
     <div className="flex justify-between">
-      <span>{text}</span><span>{correct ? "✅" : "❌"}</span>
+      <span>{text}</span>
+      <span>{correct ? "✅" : "❌"}</span>
     </div>
   );
 
-  const renderComposerCard = (text: string): ReactNode => {
+  const renderAnswerCard = (title: string, text: string, backgroundClass: string, widthRem: number, correct: boolean): ReactNode => {
     return (
-      <div className="block max-w-[12rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-cyan-500 mr-2 flex-grow mb-2">
+      <div className={`block max-w-[${widthRem}rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-${backgroundClass} mr-2 flex-grow`}>
         <div className="p-2">
           <h5 className="mb-1 text-md font-medium leading-tight text-neutral-50">
-            {renderCardTitle("Composer")}
+            {renderCardTitle(title, correct)}
           </h5>
-          <p className="text-sm leading-normal text-neutral-50">{text}</p>
-        </div>
-      </div>
-    );
-  };
-
-  const renderWorkCard = (text: string): ReactNode => {
-    return (
-      <div className="block max-w-[14rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-indigo-500 flex-grow mb-2">
-        <div className="p-2">
-          <h5 className="mb-1 text-md font-medium leading-tight text-neutral-50">
-            {renderCardTitle("Work")}
-          </h5>
-          <p className="text-sm leading-normal text-neutral-50">{text}</p>
+          <p className="text-sm leading-normal text-neutral-100">{text}</p>
         </div>
       </div>
     );
   };
 
   return (
-    <>
+    <div className="min-h-screen bg-blue-900">
       <div className="container mx-auto text-center w-3/4">
-        <h1 className="text-xl">
+        <h1 className="text-2xl text-neutral-50">
           Guess the composer and title of the following work:
         </h1>
         <div className="pb-4">
-          <img className="mx-auto outline rounded-md" src={music1} />
+          <img
+            className="mx-auto outline rounded-md"
+            src={currentPuzzle?.sheetSource}
+          />
         </div>
-        <div className="bg-gray-300 p-2">
+        <div className="bg-gray-300 p-2 rounded-sm shadow-sm">
           <form className="mb-2" onSubmit={(e) => makeGuess(e)}>
             <input
               className="mr-2 pl-1"
@@ -113,7 +112,7 @@ function App() {
               onChange={(e) => setCurrentGuess(e.target.value)}
             />
             <button
-              className="px-2 py-1 font-semibold text-sm bg-cyan-500 text-white rounded-full shadow-sm"
+              className="px-2 py-1 font-semibold text-sm bg-cyan-500 text-neutral-50 rounded-full shadow-sm"
               onClick={(e) => makeGuess(e)}
             >
               Guess
@@ -123,7 +122,7 @@ function App() {
           {renderGuesses()}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/dailyPuzzle.ts
+++ src/dailyPuzzle.ts
@@ -3,17 +3,20 @@ import sheet1 from "./assets/music-1.png";
 
 export class DailyPuzzle {
   puzzleNumber: number;
+  puzzleDate: Date;
   puzzleAnswer: ComposerWork;
   compositionYear: number;
   sheetSource: string;
 
   constructor(
     puzzleNumber: number,
+    puzzleDate: Date,
     puzzleAnswer: ComposerWork,
     compositionYear: number,
     sheetSource: string
   ) {
     this.puzzleNumber = puzzleNumber;
+    this.puzzleDate = puzzleDate;
     this.puzzleAnswer = puzzleAnswer;
     this.compositionYear = compositionYear;
     this.sheetSource = sheetSource;
@@ -23,6 +26,7 @@ export class DailyPuzzle {
 export const puzzles: DailyPuzzle[] = [
   {
     puzzleNumber: 1,
+    puzzleDate: new Date("2023-09-24"),
     puzzleAnswer: new ComposerWork(
       "Frederic Chopin",
       "Ballade No. 1 in G Minor"
