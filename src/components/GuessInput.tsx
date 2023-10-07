70dc4adb5fd50cbfd8626bbf3a1952003757938f
Reed Trevelyan <reedtrevelyan@gmail.com>
2023-10-07 17:49:31 -0400

    fix types in input component and fix opus prefix

--- src/components/GuessInput.tsx
+++ src/components/GuessInput.tsx
@@ -1,37 +1,60 @@
 import React, { useState } from "react";
-import Select from "react-select";
+import Select, { ActionMeta, OnChangeValue, StylesConfig } from "react-select";
 import worksByComposer from "../assets/parsed_composers.json";
 import { ComposerWork, getComposerWorkByID } from "./../composerWork";
 import { currentPuzzle } from "./../dailyPuzzle";
+import catalogPrefixes from "../assets/catalog_prefixes.json";
 
 type Props = {
     onSubmit: (guess: ComposerWork) => void;
 };
 
+type ChoiceOption = {
+    value: number;
+    label: string;
+    isFixed: boolean;
+};
+
+function hasKey<O extends object>(obj: O, key: PropertyKey): key is keyof O {
+    return key in obj;
+}
+
 function GuessInput({ onSubmit }: Props) {
+    const renderWorkChoiceLine = (work: ComposerWork, composerID: number): string => {
+        let prefix = "Op. ";
+        if (hasKey(catalogPrefixes, composerID)) {
+            prefix = catalogPrefixes[composerID];
+        }
+
+        const opusString = `(${prefix}${work.opus}${work.opusNumber && work.opusNumber >= 0 ? ` #${work.opusNumber}` : ""})`;
+        const workValue = `${opusString} ${work.work}`;
+
+        return workValue;
+    };
+
     const allComposerOptions = worksByComposer.map((composer) => {
         return {
             value: composer.id,
             label: composer.fullname,
+            isFixed: false,
         };
     });
-    const composerMap: { [key: string]: { value: number; label: string }[] } = {};
+    const composerMap: { [key: string]: ChoiceOption[] } = {};
 
     for (const entry of worksByComposer) {
         composerMap[entry.id] = entry.works.map((work) => {
-            const opusString = `(${work.opus} ${work.opus_number >= 0 ? `#${work.opus_number}` : ""
-                })`;
-            const workValue = `${opusString} ${work.work_title}`;
+            const cw = getComposerWorkByID(entry.id, work.id);
 
             return {
                 value: work.id,
-                label: workValue,
+                label: renderWorkChoiceLine(cw, entry.id),
+                isFixed: false,
             };
         });
     }
 
-    const [currentOptions, setCurrentOptions] = useState(allComposerOptions);
-    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [currentOptions, setCurrentOptions] = useState<readonly ChoiceOption[]>(allComposerOptions);
+    const [selectedOptions, setSelectedOptions] = useState<readonly ChoiceOption[]>([]);
     const [placeholderText, setPlaceholderText] = useState("Enter composer...");
 
     const resetToInitial = () => {
@@ -39,19 +62,47 @@ function GuessInput({ onSubmit }: Props) {
         setSelectedOptions([]);
     }
 
+    const orderOptions = (values: readonly ChoiceOption[]) => {
+        return values
+            .filter((v) => v.isFixed)
+            .concat(values.filter((v) => !v.isFixed));
+    };
+
     const isComposerCorrect = (composer: string): boolean => composer === currentPuzzle.puzzleAnswer.composer;
 
-    const onSelectChange = (option) => {
+    const onSelectChange = (option: OnChangeValue<ChoiceOption, true>, actionMeta: ActionMeta<ChoiceOption>) => {
         if (option.length === 0) {
             resetToInitial();
             return;
         }
 
-        console.log(option);
+        switch (actionMeta.action) {
+            case 'remove-value':
+            case 'pop-value':
+                if (actionMeta.removedValue.isFixed) {
+                    return;
+                }
+                break;
+        }
+
         const composer = option[0].value;
         const workOptions = composerMap[composer];
         setCurrentOptions(workOptions);
-        setSelectedOptions(option);
+        setSelectedOptions(orderOptions(option));
+    };
+
+    const styles: StylesConfig<ChoiceOption, true> = {
+        multiValue: (base, state) => {
+            return state.data.isFixed ? { ...base, backgroundColor: '#06b6d4', borderRadius: "4px" } : base;
+        },
+        multiValueLabel: (base, state) => {
+            return state.data.isFixed
+                ? { ...base, fontWeight: 'bold', color: 'white', paddingRight: 6 }
+                : base;
+        },
+        multiValueRemove: (base, state) => {
+            return state.data.isFixed ? { ...base, display: 'none' } : base;
+        },
     };
 
     return (
@@ -72,7 +123,9 @@ function GuessInput({ onSubmit }: Props) {
                     setPlaceholderText("");
                 } else if (isComposerCorrect(guess.composer)) {
                     // set options back to works, and preserve the selected composer
-                    setSelectedOptions(selectedOptions[0]);
+                    const fixedComposer = selectedOptions[0];
+                    fixedComposer.isFixed = true;
+                    setSelectedOptions([fixedComposer]);
 
                     const workOptions = composerMap[composerID];
                     setCurrentOptions(workOptions);
@@ -88,6 +141,8 @@ function GuessInput({ onSubmit }: Props) {
                 placeholder={placeholderText}
                 className="w-3/4 md:w-2/3 mr-2"
                 value={selectedOptions}
+                isClearable={false}
+                styles={styles}
             />
 
             <button
