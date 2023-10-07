b5f249537b9c9a448386156f0808e77c24f9631d
Reed Trevelyan <reedtrevelyan@gmail.com>
2023-10-06 23:13:54 -0400

    dynamic tailwind color fix

--- src/components/GuessDateIndicator.tsx
+++ src/components/GuessDateIndicator.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 function GuessDateIndicator({ guessYear, targetYear, composerCareerLength }: Props) {
-    const guessDiffToColor = (): number => {
+    const guessDiffToColor = (): string => {
         const diffAbs = Math.abs(guessYear - targetYear);
         const diffPercent = diffAbs / composerCareerLength;
 
@@ -16,8 +16,22 @@ function GuessDateIndicator({ guessYear, targetYear, composerCareerLength }: Pro
             steps.push(i);
         }
 
-        const choice = steps[steps.length - Math.floor(steps.length * diffPercent) - 1]
-        return choice;
+        const choice = steps[steps.length - Math.floor(steps.length * diffPercent) - 1];
+
+        // tailwind sucks
+        const classes: { [choice: number]: string } = {
+            100: "bg-sky-100",
+            200: "bg-sky-200",
+            300: "bg-sky-300",
+            400: "bg-sky-400",
+            500: "bg-sky-500",
+            600: "bg-sky-600",
+            700: "bg-sky-700",
+            800: "bg-sky-800",
+            900: "bg-sky-900",
+        };
+
+        return classes[choice];
     };
 
     const getArrow = (): string => {
@@ -30,9 +44,8 @@ function GuessDateIndicator({ guessYear, targetYear, composerCareerLength }: Pro
         }
     };
 
-    const colorStr = `bg-sky-${guessDiffToColor()} w-[2rem] rounded-lg py-1 px-2 mr-2 text-blue-800 text-md font-semibold flex flex-col justify-center content-center`
     return (
-        <div className={colorStr}>
+        <div className={`${guessDiffToColor()} w-[2rem] rounded-lg py-1 px-2 mr-2 text-blue-800 text-md font-semibold flex flex-col justify-center content-center`}>
             <span>{getArrow()}</span>
         </div>
     )
--- src/components/GuessRow.tsx
+++ src/components/GuessRow.tsx
@@ -30,7 +30,9 @@ function GuessRow({ rowNumber, guess }: { rowNumber: number, guess: ComposerWork
                 widthRem={14}
                 correct={puzzleAnswer.matchWork(guess.work)}
             />
-            <GuessDateIndicator guessYear={guess.compositionYear} targetYear={puzzleAnswer.compositionYear} composerCareerLength={yearRange} />
+            {composerCorrect(guess) ?
+                <GuessDateIndicator guessYear={guess.compositionYear} targetYear={puzzleAnswer.compositionYear} composerCareerLength={yearRange} />
+                : <div className="block max-w-[2rem] rounded-lg text-left shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-slate-400 mr-2 flex-grow"></div>}
         </GuessRowContainer>
     )
 }
