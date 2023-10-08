cf48b1370ba2ae894f418a1adee871ea6879a05f
Reed Trevelyan <reedtrevelyan@gmail.com>
2023-10-07 20:11:01 -0400

    dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     const renderDropdown = () => {
         const displayClass = dropdownOpen ? "block" : "hidden";
         return (
-            <div className="relative ml-2">
+            <div className="relative ml-2 mr-0 md:mr-4">
                 <a
                     className="flex items-center px-6 pb-2 pt-2.5 text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                     href="#"
@@ -15,7 +15,7 @@ function Header() {
                     aria-expanded="false"
                     onClick={() => setDropdownOpen(!dropdownOpen)}
                 >
-                    Dropdown link
+                    Select music
                     <span className="ml-2 w-2">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
@@ -30,26 +30,26 @@ function Header() {
                     </span>
                 </a>
                 <ul
-                    className={`${displayClass} absolute z-[1000] float-left m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block`}
+                    className={`${displayClass} absolute z-[1000] m-0 min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block`}
                     aria-labelledby="dropdownMenuButton2"
                 >
                     <li>
                         <a
                             className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                             href="#"
-                        >Action</a>
+                        >Piano</a>
                     </li>
                     <li>
                         <a
                             className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                             href="#"
-                        >Another action</a>
+                        >Violin</a>
                     </li>
                     <li>
                         <a
                             className="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
                             href="#"
-                        >Something else here</a>
+                        >Orchestral</a>
                     </li>
                 </ul>
             </div>
@@ -57,7 +57,7 @@ function Header() {
     };
     return (
         <nav
-            className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600 lg:py-4"
+            className="relative flex w-full flex-wrap items-center justify-between bg-[#FBFBFB] py-2 text-neutral-500 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-neutral-600"
             data-te-navbar-ref>
             <div className="flex w-full flex-wrap items-center justify-between px-3">
                 <div className="ml-2">
