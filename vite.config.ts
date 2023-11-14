afa12d54fd6a90dc020b7492c75e5a219142178a
Reed Trevelyan <reedtrevelyan@gmail.com>
2023-11-13 21:18:26 -0500

    add cname record

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         missing: resolve(__dirname, '404.html'),
+        cname: resolve(__dirname, 'CNAME'),
       },
     },
   }
