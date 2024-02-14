8d70442865258b4907dd93b1f7e7f78ebd21cfeb
Reed Trevelyan <reedtrevelyan@gmail.com>
2024-02-14 00:22:37 -0500

    fix footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className=" dark:bg-neutral-600 bg-[#FBFBFB] container mx-auto text-neutral-200 mt-4 py-1 px-4 border-t-2 border-neutral-500 flex justify-between">
+        <div className=" dark:bg-neutral-600 bg-[#FBFBFB] min-w-fullcontainer text-neutral-200 mt-4 py-1 px-4 border-t-2 border-neutral-500 flex justify-between">
             <span>Made with 🎻 by <a className="underline decoration-dashed underline-offset-4" href="https://github.com/Heanthor">@Heanthor</a></span>
             <span>© {new Date().getFullYear()}</span>
         </div>
